e6f6dabcc75806d2cebbf3e768ca4e9f3e4c6a45
Praveen Kumar G <praveenkumargpraveenkumarg85@gmail.com>
2024-04-04 04:02:02 +0530

    added chating featues

--- src/components/MyChats.jsx
+++ src/components/MyChats.jsx
@@ -1,8 +1,33 @@
 import { Box, Text } from "@chakra-ui/layout";
 import GroupChatModal from "./GroupChatModal";
+import { useEffect, useState } from "react";
+import axios from "axios";
 // import ProfileModal from "./ProfileModal";
 
 const MyChats = () => {
+  const headers = {
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
+    "Content-Type": "application/json",
+  };
+  const [allChat, setallChat] = useState([]);
+
+  const alllchathanlder = async () => {
+    const { data } = await axios.get("http://127.0.0.1:8000/chat/getAll", {
+      headers: headers,
+    });
+    if (data) {
+      setallChat(data.data);
+    }
+  };
+  const handlergetMessages = async (chatId) => {
+    console.log("hi");
+    console.log(chatId);
+  };
+
+  useEffect(() => {
+    alllchathanlder();
+  }, []);
+
   return (
     <Box
       d="flex"
@@ -12,12 +37,34 @@ const MyChats = () => {
       w="31%"
       borderRadius="lg"
       borderWidth="1px"
+      height={"602px"}
     >
-      <Text fontSize="4xl" fontFamily="Work sans">
-        My Chats
-        <GroupChatModal />
-        {/* <ProfileModal/> */}
-      </Text>
+      <u>
+        {" "}
+        <Text fontSize="28px" fontFamily="Work sans">
+          my chats
+        </Text>
+      </u>
+      <Box overflowY={"scroll"} height={"550px"}>
+        {allChat &&
+          allChat.length != 0 &&
+          allChat.map((el, index) => (
+            <Box
+              key={el.id} // Make sure to provide a unique key for each rendered element
+              border={"1px solid black"}
+              padding={"10px"}
+              borderRadius={"10px"}
+              onClick={(e) => {
+                e.preventDefault();
+                handlergetMessages(el.id);
+              }}
+              margin={"4px"}
+            >
+              <h2>{el.chatname}</h2>{" "}
+              <p>{!el.isGroupChat && el.users[1].email}</p>{" "}
+            </Box>
+          ))}
+      </Box>
     </Box>
   );
 };
--- src/components/SideDrawer.jsx
+++ src/components/SideDrawer.jsx
@@ -2,6 +2,7 @@ import { Button } from "@chakra-ui/button";
 import { useDisclosure } from "@chakra-ui/hooks";
 import { Input } from "@chakra-ui/input";
 import { Box, Text } from "@chakra-ui/layout";
+// axios
 import {
   Drawer,
   DrawerBody,
@@ -10,9 +11,57 @@ import {
   DrawerOverlay,
 } from "@chakra-ui/modal";
 import { Tooltip } from "@chakra-ui/tooltip";
+import axios, { isCancel } from "axios";
+import { useState } from "react";
+
+
 
 function SideDrawer() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+   const [userSearch, setuserSearch] = useState('');
+  //  const [isOpen,setisOpen]=useState(false);
+   const [searchedUsers, setsearchedUsers] = useState([]);
+  //  console.log(userSearch);
+  const headers = {
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+    "Content-Type": "application/json",
+  };
+  // console.log(headers);
+
+   const handlerUserSearch=async(e)=>{
+       e.preventDefault();
+      //  console.log("hi");
+      const data = await axios.get("http://127.0.0.1:8000/chat/findUser", {
+        headers: headers,
+        params: { name: userSearch },
+      });
+     
+        setsearchedUsers(data.data.data);
+    
+
+   }
+   const handlerChatcreating=async(user)=>{
+       
+         const {data} = await axios.post(
+           "http://127.0.0.1:8000/chat/create",
+           {
+             chatname: user.username,
+             userId: user.userId,
+             isGroupChat: false,
+           },
+          {
+            headers:headers
+          }
+         );
+
+       if(data){
+         
+        onClose();
+       }
+  
+       
+   }
+  //  console.log(searchedUsers);
 
   return (
     <>
@@ -28,11 +77,11 @@ function SideDrawer() {
       >
         <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
           <Button variant="solid" onClick={onOpen}>
-            <i class="fas fa-search"></i>
+            search
           </Button>
         </Tooltip>
         <Text fontSize="2xl" fontFamily="Work sans">
-          MERN CHAT APP
+          study with friends
         </Text>
       </Box>
 
@@ -41,7 +90,31 @@ function SideDrawer() {
         <DrawerContent>
           <DrawerHeader borderBottomWidth="1px">Search Users</DrawerHeader>
           <DrawerBody>
-            <Input placeholder="Search by username" />
+            <Input
+              placeholder="Search by username"
+              onChange={(e) => setuserSearch(e.target.value)}
+            />
+            <Button backgroundColor={"lightblue"} onClick={handlerUserSearch}>
+              search
+            </Button>
+
+            {searchedUsers &&
+              searchedUsers.length !== 0 &&
+              searchedUsers.map((user, index) => (
+                <Box
+                  key={user.userId} // Make sure to provide a unique key for each rendered element
+                  border={"1px solid black"}
+                  padding={"10px"}
+                  borderRadius={"10px"}
+                  onClick={(e)=>{
+                    e.preventDefault();
+                    handlerChatcreating(user)}}
+                >
+                  <h2>{user.username}</h2>{" "}
+                  <p>{user.email}</p>{" "}
+                 
+                </Box>
+              ))}
           </DrawerBody>
         </DrawerContent>
       </Drawer>
